49fbeea55007ca0ae19668017c5bd3030593e6d8
z0mbie42 <sylvain.kerkour@gmail.com>
2018-09-06 23:14:05 +0200

    working

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
+
 import App from './App.vue';
 import router from './router';
+
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,18 +1,145 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-layout row wrap style="height: 80vh;">
+      <v-flex xs10 offset-xs1>
+        <v-layout row wrap>
+        <v-flex xs10>
+        <v-text-field
+          label="url"
+          single-line
+          outline
+          v-model="url"
+          @keyup.enter.native="go()"
+          ></v-text-field>
+
+        </v-flex>
+        <v-flex xs2>
+          <v-btn color="primary" :loading="loading" :disabled="!canGo || loading" @click="go()">
+            Go
+          </v-btn>
+        </v-flex>
+        </v-layout>
+      </v-flex>
+
+      <div id="chart"></div>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Highcharts from 'highcharts';
+import Vue from 'vue';
+import axios from 'axios';
+
+const locations = [
+  'z0mbie42-latency-eu',
+  'z0mbie42-latency-us',
+];
+
+function getURLs(url) {
+  return [url, url].map((u, i) => `https://${locations[i]}.herokuapp.com/${u}`);
+}
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
+  data: () => ({
+    chart: null,
+    url: '',
+    loading: false,
+    data: null,
+  }),
+  computed: {
+    canGo() {
+      const pattern = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/; // eslint-disable-line
+      if (!pattern.test(this.url)) {
+        return false;
+      }
+
+      return true;
+    },
+  },
+  watch: {
+    url(val) {
+      this.$router.replace({ query: { url: val } });
+    },
+  },
+  created() {
+    this.url = this.$route.query.url;
+  },
+  methods: {
+    async go() {
+      this.loading = true;
+      let data = await Promise.all(getURLs(this.url).map(u => axios.get(u)));
+      data = data.map(d => d.data.data);
+      console.log(data);
+      Vue.nextTick(() => {
+        this.renderChart(data);
+        this.loading = false;
+      });
+    },
+    reflowChart() {
+      setTimeout(() => {
+        if (this.chart) {
+          this.chart.reflow();
+        }
+      }, 1500);
+    },
+    renderChart(data) {
+      this.chart = Highcharts.chart('chart', {
+        chart: {
+          type: 'bar',
+        },
+        title: {
+          text: 'Latency',
+        },
+        xAxis: {
+          categories: ['us', 'eu'],
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: null,
+          },
+          type: 'datetime',
+        },
+        legend: {
+          reversed: true,
+        },
+        plotOptions: {
+          series: {
+            stacking: 'normal',
+          },
+        },
+        tooltip: {
+          valueSuffix: ' ms',
+        },
+        series: [{
+          name: 'DNS Lookup',
+          data: [data[0].dns_lookup, data[1].dns_lookup],
+        }, {
+          name: 'TCP Connection',
+          data: [data[0].tcp_connection, data[1].tcp_connection],
+        }, {
+          name: 'TLS Handshake',
+          data: [data[0].tls_handshake, data[1].tls_handshake],
+        }, {
+          name: 'Server Processing',
+          data: [data[0].server_processing, data[1].server_processing],
+        }, {
+          name: 'Content Transfer',
+          data: [data[0].content_transfer, data[1].content_transfer],
+        }],
+      });
+    },
   },
 };
 </script>
+
+<style scoped>
+
+#chart {
+  width: 100%;
+}
+
+</style>
